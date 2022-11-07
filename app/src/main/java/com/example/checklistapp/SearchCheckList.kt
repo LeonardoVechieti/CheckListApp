ec7d64f44a3ebe5891f51ff85596898958179962
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2022-11-06 23:30:49 -0300

    Feat: Update satutus

---
+++ app/src/main/java/com/example/checklistapp/SearchCheckList.kt
@@ -0,0 +1,11 @@
+package com.example.checklistapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchCheckList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_check_list)
+    }
+}
